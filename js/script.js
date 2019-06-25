ee8da3f0302c75ba649fbde6d9631eb2a7308098
Davidpark27 <52177115+Davidpark27@users.noreply.github.com>
2019-06-24 20:02:17 -0700

    final v1
    finished meets expectations.

--- js/script.js
+++ js/script.js
@@ -1,95 +1,84 @@
+//Random quote generator. 
+
+
+//array of quote objects, sources, year, citation. 
 var quotes = [
-  {quote: 
-  source:
-  year:
+  {quote : 'It\'s Not Whether You Get Knocked Down, It\'s Whether You Get Up.' ,
+  source : 'Vince Lombardi',
+  
 },
-  {quote: 
-  source:
-  year:
+  {quote : 'We May Encounter Many Defeats But We Must Not Be Defeated.',
+  source : "Maya Angelou",
+  
   },
-  {quote: 
-  source:
-  year:
+  {quote : 'Do the difficult things while they are easy and do the great things while they are small. A journey of a thousand miles must begin with a single step.',
+  source : 'Lao Tzu',
+  year : 'Long ago'
   },
-  {quote: 
-  source:
-  year:
+  {quote : 'The artist is nothing without the gift, but the gift is nothing without work.',
+  source : 'Emile Zola',
+  year : '1840-1902'
   },
-  {quote: 
-  source:
-  year:
+  {quote : 'In theory, there is no difference between theory and practice. But in practice, there is.',
+  source : 'Yogi Berra',
+ 
   },
-  {quote: 
-  source:
-  year:
+  {quote : 'Each problem that I solved became a rule which served afterwards to solve other problems.',
+  source : 'Rene Descartes',
+  year : '1596-1650'
   },
-  {quote: 
-  source:
-  year:
+  {quote : 'Do, or do not. There is no try.',
+  source : 'Yoda',
+  year : 'Long time ago',
+  citation : 'Star Wars: Empire Strikes Back'
   },
 
 ];
 
+console.log(quotes);
 
 
 
+//get random quote function. Makes a random number from 0-6. returns the 
+// quote from quotes array according to randomNumber.0-6
+
+function getRandomQuote () {
+  var randomNumber = Math.floor( Math.random () * 7);
+  return quotes[randomNumber]
+};
+
+//just testing function
+var rquote = getRandomQuote ();
+console.log (rquote.source);
 
 
+//Print quote function. html starts as an empty string.  Random quotes, sources, years are inserted into html variable using conditional statements.
+// Html variable is printed by document.getElementById. 
+
+function printQuote () {
+  var html = '';
+  var randomQuote = getRandomQuote();
+  html += "<p class = 'quote'>" + randomQuote.quote + "</p>";
+  html += "<p class = 'source'>" + randomQuote.source ;
+  if (randomQuote.citation) {
+    html += "<span class = 'citation' >" + randomQuote.citation + "</span>";
+    
+  }
+  if (randomQuote.year) {
+    html += "<span class = 'year ' >" + randomQuote.year + "</span>";
+  
+   
+  }
+  html += '</p>';
+  document.getElementById('quote-box').innerHTML = html;
+
+};
+
+//added so new quote is displayed when page is refreshed. 
+printQuote ();
 
 
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
-
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
-
-
-
-
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
-
-
-
-
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
-
-
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
-
+//Script for the button
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
